575f23fe7fb105df528136618ab2597e66c96e3c
Henrique Gomes <henriqueglp@gmail.com>
2019-01-09 10:13:07 -0200

    Refactoring results list

--- src/components/Table/Table-item.js
+++ src/components/Table/Table-item.js
@@ -2,18 +2,27 @@
 import React from 'react'
 import StarIcon from '@material-ui/icons/Star'
 
+const parseStrToHTML = (str) => {
+    var parser = new DOMParser()
+    var doc    = parser.parseFromString(str, "text/html")
+
+    return doc.childNodes[0].innerText
+}
+
 const TableItem = props => {
     return (
         <li className="row">
             <p className="name">
-                <StarIcon id="star-icon" className="" />{props.name}
+                <button className="star-btn">
+                    <StarIcon id="star-icon" className="" />
+                </button>
+                {props.name}
             </p>
-            <p>
-                {props.description}
+            <p className="description">
+                {parseStrToHTML(props.description)}
             </p>
         </li>
     )
 }
 
 export default TableItem
-
