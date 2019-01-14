26891d5da04444905a876def3b9943a8e4277d4d
Henrique Gomes <henriquegomeslp@yahoo.com.br>
2019-01-14 16:20:40 -0200

    Updates

---
+++ precache-manifest.19863a6596418ce1bf7dd401466581ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8db8d939d1a06765800",
+    "url": "/waste-wizard/static/css/main.194094a4.chunk.css"
+  },
+  {
+    "revision": "a8db8d939d1a06765800",
+    "url": "/waste-wizard/static/js/main.a8db8d93.chunk.js"
+  },
+  {
+    "revision": "c4ac21136774b5f079cd",
+    "url": "/waste-wizard/static/js/1.c4ac2113.chunk.js"
+  },
+  {
+    "revision": "1f63a71094396a3192a6",
+    "url": "/waste-wizard/static/js/runtime~main.1f63a710.js"
+  },
+  {
+    "revision": "03e6cc03f8c4b4708d4f48b1c81a9d82",
+    "url": "/waste-wizard/index.html"
+  }
+];
